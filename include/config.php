b4fb8675af1d5d7c5b9b6b2a9a18395115e4cb14
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-08 15:05:24 +0000

    Add Error Handler

--- include/config.php
+++ include/config.php
@@ -9,4 +9,23 @@
 	define('TEMPLATE_DIR', PRESENTATION_DIR . 'templates');
 	define('COMPILE_DIR', PRESENTATION_DIR . 'templates_c');
 	define('CONFIG_DIR', SITE_ROOT . '/include/configs');
+
+	// These should be true whilst in development mode
+	define('IS_WARNING_FATAL', true);
+	define('DEBUGGING', true);
+
+	// The error types to be reported
+	define('ERROR_TYPES', E_ALL);
+
+	// Mail the error messages to Guru Computers Staff
+	define('SEND_ERROR_MAIL', false);
+	define('ADMIN_ERROR_MAIL', 'admin@gurucomputers.co.uk');
+	define('SENDMAIL_FROM', 'errors@gurucomputers.co.uk');
+	ini_set('sendmail_from', SENDMAIL_FROM);
+
+	// By Default we don't log errors to a file
+	define('LOG_ERRORS', false);
+	define('LOG_ERRORS_FILE', '/var/log/shoperror.log');
+	// Generic Error Message instead of debug info
+	define('SITE_GENERIC_ERROR_MESSAGE', '<h1>Oops, something went wrong!</h1?');
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 	// Include Utility Files
 	require_once 'include/config.php';
+	require_once BUSINESS_DIR . 'error_handler.php';
+
+	// Set the error handler
+	ErrorHandler::SetHandler();
 
 	// Load the application page template
 	require_once PRESENTATION_DIR . 'application.php';
